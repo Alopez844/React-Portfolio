f0a64090bd3257f91664d576cc668cc60d4f4e61
unknown <lopez1432.al@gmail.com>
2022-03-11 13:24:31 -0800

    moved components and assest folder into src folder and completed header footer and about index JS files

---
+++ alejandros-portfolio/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div class="footer" id='footer'>
+        <a href="https://github.com/alopez844"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" class="icon"/></a>
+        <a href="https://www.linkedin.com/in/alex-lopez-8aa63816a/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" class="icon"/></a>
+        <a href="https://www.instagram.com/alejandro.lopez1432/"><img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png" alt="Instagram" class="icon"/></a> 
+
+        <p>
+            ACL Code | made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
+        </p> 
+        
+    </div>
+  )
+}
---
+++ alejandros-portfolio/src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {Route, Redirect, HashRouter} from "react-router-dom";
+import About from '../../components/About';
+
+class Header extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <div className='row Header' id='header'>
+                    <a href='link to github page'><img src='../../assets/portfolio-logo.png' alt='logo' class='icon' id='logo'/></a>    
+                    <Nav/>
+                </div>
+
+                <div className='content'>
+                    <Route exact path='/' render={() => (<Redirect to='/portfolio'/>)}/>
+                    <Route path='/portfolio' component={Portfolio}/>
+                    <Route path='/about' component={About}/>
+                    <Route path='/contact' component={Contact}/>
+                    <Route path='/resume' component={Resume}/>
+                </div>
+
+            </HashRouter>
+        )
+    }
+}
+
+export default Header;
